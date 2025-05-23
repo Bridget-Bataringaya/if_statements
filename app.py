1f4fcdc9b6369793698047663e4da4e19a96c821
Bridget-Bataringaya <162678895+Bridget-Bataringaya@users.noreply.github.com>
2025-05-23 04:45:57 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import random
+
+print("Welcom to python programming")
+print("i am Bridget i love python programming")
+
+
+x=5
+y=17
+
+x=("john")
+print(x)
+print(y)
+
+
+print(type(x))
+print(type(y))
+
+#THE GUESS RANDOM NUMBER GAME
+secret_number=6
+
+users_guess= int(input("ENTER THE NUMBER :"))
+
+
+
