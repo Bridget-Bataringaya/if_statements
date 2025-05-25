c18b5d4a96093ff68e83d3a5e9e2a3202c0a41d0
Bridget-Bataringaya <Bridget-Bataringaya>
2025-05-25 20:52:48 +0300

    Initial commit

---
+++ WEEK ONE/Variables.py
@@ -0,0 +1,11 @@
+x=5
+y=6
+z=8
+
+print(x,y,z)
+
+print("i am bridget i love python programming")
+
+if y>x :
+    print("y is greater than x")
+
---
+++ WEEK ONE/if_statements.py
@@ -0,0 +1,29 @@
+#equal a == b
+#not equal a != b
+#greater than a > b
+#less than a < b
+#greater than or equal to a >= b
+#less than or equal to a <= b
+
+
+
+#if statements
+
+age = 20
+if age >= 18:
+    print("you are an adult")
+elif age >=13:
+    print("you are a teenager") 
+else:
+    print("you are a child") 
+    
+    #EXERCISE 1
+    #.QN Grade basic using if statements, elif statement and else statement
+    
+marks=56
+if marks >= 75:
+        print("GOOD PERFORMANCE")
+elif marks >= 50:
+        print("AVERAGE PERFORMANCE")
+else:
+        print("POOR PERFORMANCE")              
