dc478521b46638c1dc7f62f6bff7498f5a199225
Bridget-Bataringaya <Bridget-Bataringaya>
2025-05-26 03:30:28 +0300

    my assignment of find  factorial of 5

---
+++ WEEK ONE/assigment_3.py
@@ -0,0 +1,9 @@
+import math
+#function to find the factorial of 5
+
+def factorialOf5():
+    print ("The factorial of 5 is : " ,math.factorial(5)) 
+    
+    
+    
+factorialOf5()
---
+++ WEEK ONE/loops.py
@@ -0,0 +1,6 @@
+#loops(for,while)
+#loops are used to iterate over a sequence
+#(like a list, tuple, dictonary, set or string)or repeat a block of code multiple time
+
+for i in range(1,5):
+   print(i)
