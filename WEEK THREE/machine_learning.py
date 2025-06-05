f6e2388692f9fb87ec7ed31e3db7fc98e1a5a2a6
Bridget-Bataringaya <Bridget-Bataringaya>
2025-06-05 12:03:57 +0300

    exercise

---
+++ WEEK THREE/machine_learning.py
@@ -0,0 +1,44 @@
+#machine learning
+#introduction to machine learning
+#what  is machine learning
+
+
+#applications of machine learning
+#1.image analysis
+#2.speech recognition
+#3.disease prediction
+#4.stock market analysis
+#5.fraud detection
+#6.recommendation systems
+
+
+#types of machine learning
+#1.supervised learning  --llabelled dataset, structured
+#2.unsupervised learning
+#3. semi supervised learning
+#4.reinforment learning
+
+'''
+COMMON ALGORITHMS IN SUPERVISED LEARNING
+-linear regression
+-logistic regression
+=decision trees
+-support vector machines(SVM)
+-K-Nearest neighbor(KNN)
+-Random Forest(RF)
+
+
+-DEEP LEARNING(CNN)-image classification
+
+'''
+
+#2. unsupervised learning -unlabeled data,discovering hidden pattrens,clusters,structures
+'''
+-k-mans clustering
+-hierarchical clustering
+-PCA
+
+
+'''
+
+#MODEL SELECTION AND VALIDATION
---
+++ WEEK THREE/train_model.py
@@ -0,0 +1,6 @@
+#import required libraries
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras 
+from tensorflow.keras.processing.image import ImageDataGenerator
